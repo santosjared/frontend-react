3823e93fc8f3c02e2740eb3e981c0f4212f0c775
William Barra <wbarra-ext@wondrhealth.com>
2025-01-07 21:57:20 -0400

    create Default component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Default from "./Components/Default.jsx";
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
-      <h2>Diplomado en fullstack</h2>
-      <h3>Mod 7: Curso de React</h3>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Default />
     </>
   )
 }
