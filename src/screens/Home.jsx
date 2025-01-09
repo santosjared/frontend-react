3dd80f69a7b4e103e05670c09cee99c82961afcb
William Barra <wbarra-ext@wondrhealth.com>
2025-01-08 21:46:03 -0400

    Manage props

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,26 @@
-import Logo from '../Components/Home/Logo.jsx';
+import OpenLink from "../Components/Core/OpenLink.jsx";
 import Body from '../Components/Home/Body.jsx';
 import Footer from '../Components/Home/Footer.jsx';
-import GitHub from "../Components/Home/GitHub.jsx";
+
+import viteLogo from "../assets/vite.svg";
+import reactLogo from "../assets/react.svg";
+import githubLogo from "../assets/github.svg";
 
 const Home = () => {
   return (
       <div>
-          <Logo />
-          <GitHub />
+          <OpenLink
+              redirectURL="https://vite.dev"
+              logoImage={viteLogo}
+          />
+          <OpenLink
+              redirectURL="https://react.dev"
+              logoImage={reactLogo}
+          />
+          <OpenLink
+              redirectURL="https://github.com/dheeyi/desarrollo-frontend-react-2025"
+              logoImage={githubLogo}
+          />
           <Body />
           <Footer />
       </div>
