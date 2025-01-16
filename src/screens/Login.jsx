c48c90ede6a629b8bb50da04721a8017d6750487
William <dheeyi@gmail.com>
2025-01-15 21:55:30 -0400

    adds forms. hooks and animations

---
+++ src/screens/Login.jsx
@@ -0,0 +1,13 @@
+// import FormLogin from '../Components/Forms/FormLogin.jsx';
+// import FormLoginWithHook from "../Components/Forms/FormLoginWithHook.jsx";
+import FormLoginWithMotion from "../Components/Forms/FormLoginWithMotion.jsx";
+
+const Login = () => {
+    return (
+        <>
+            <FormLoginWithMotion titleForm="Welcome to FullStack program" />
+        </>
+    );
+};
+
+export default Login;
