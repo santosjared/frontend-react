07a283e11f57d61ad3d27dbec9f27cfbd5456b13
Santos <santosjared221234@gmail.com>
2025-01-25 14:18:55 -0400

    fix repository

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Provider store={store}>
-          <BrowserRouter basename="/desarrollo-frontend-react-2025/">
+          <BrowserRouter basename="/frontend-react/">
             <App />
           </BrowserRouter>
       </Provider>
